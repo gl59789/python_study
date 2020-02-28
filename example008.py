be9fc22bcbc8cf8818df3f8d5c2f9ab22e9ee2af
Kevin Lee <kelee@riotgames.com>
2020-02-28 12:55:20 +0900

    regex example 008

--- example008.py
+++ example008.py
@@ -1,6 +1,10 @@
-#!/usr/bin/env python3
+import re
+def check_sentence(text):
+    result = re.search(r"^[A-Z][A-Za-z ]*[?.!]$", text)
+    return result != None
 
-def main():
-    pass
-
-main()
+print(check_sentence("Is this is a sentence?")) # True
+print(check_sentence("is this is a sentence?")) # False
+print(check_sentence("Hello")) # False
+print(check_sentence("1-2-3-GO!")) # False
+print(check_sentence("A star is born.")) # True
