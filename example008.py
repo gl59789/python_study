429e735e517b0810b10e1d9648a79366f5db4422
Kevin Lee <kelee@riotgames.com>
2020-03-02 20:14:32 +0900

    More regex example -2

--- example008.py
+++ example008.py
@@ -45,7 +45,7 @@ def long_words(text):
 print(long_words("I like to drink coffee in the morning.")) # ['morning']
 print(long_words("I also have a taste for hot chocolate in the afternoon.")) # ['chocolate', 'afternoon']
 print(long_words("I never drink tea late at night.")) # []
-"""
+
 
 import re
 def extract_pid(log_line):
@@ -59,3 +59,54 @@ print(extract_pid("July 31 07:51:48 mycomputer bad_process[12345]: ERROR Perform
 print(extract_pid("99 elephants in a [cage]")) # None
 print(extract_pid("A string that also has numbers [34567] but no uppercase message")) # None
 print(extract_pid("July 31 08:08:08 mycomputer new_process[67890]: RUNNING Performing backup")) # 67890 (RUNNING)
+
+
+import re
+def convert_phone_number(phone):
+    result = re.sub(r"(\d{3})-(\d{3})-(\d{4})\b", r"(\1) \2-\3", phone)
+    return result
+
+print(convert_phone_number("My number is 212-345-9999.")) # My number is (212) 345-9999.
+print(convert_phone_number("Please call 888-555-1234")) # Please call (888) 555-1234
+print(convert_phone_number("123-123-12345")) # 123-123-12345
+print(convert_phone_number("Phone number of Buckingham Palace is +44 303 123 7300")) # Phone number of Buckingham Palace is +44 303 123 7300
+
+
+import re
+def transform_record(record):
+    new_record = re.sub(r"(\d{3})-(\d{3})", r"1-\1-\2", record)
+    return new_record
+
+print(transform_record("Sabrina Green,802-867-5309,System Administrator"))
+# Sabrina Green,+1-802-867-5309,System Administrator
+
+print(transform_record("Eli Jones,684-3481127,IT specialist"))
+# Eli Jones,+1-684-3481127,IT specialist
+
+print(transform_record("Melody Daniels,846-687-7436,Programmer"))
+# Melody Daniels,+1-846-687-7436,Programmer
+
+print(transform_record("Charlie Rivera,698-746-3357,Web Developer"))
+# Charlie Rivera,+1-698-746-3357,Web Developer
+"""
+
+import re
+def multi_vowel_words(text):
+    pattern = r"([aeiou]{3-})"
+    result = re.findall(pattern, text)
+    return result
+
+print(multi_vowel_words("Life is beautiful"))
+# ['beautiful']
+
+print(multi_vowel_words("Obviously, the queen is courageous and gracious."))
+# ['Obviously', 'queen', 'courageous', 'gracious']
+
+print(multi_vowel_words("The rambunctious children had to sit quietly and await their delicious dinner."))
+# ['rambunctious', 'quietly', 'delicious']
+
+print(multi_vowel_words("The order of a data queue is First In First Out (FIFO)"))
+# ['queue']
+
+print(multi_vowel_words("Hello world!"))
+# []
